78e572735f3ca2d0ff851fc9a430f1f2e53153f7
glazyp <pihoovm@gmail.com>
2019-10-20 00:31:00 +0530

    Prime factors
    removed for statement, changed initial value of i and the value of i%j

--- Prime Factors/primefactorization.py
+++ Prime Factors/primefactorization.py
@@ -1,15 +1,18 @@
-n=int(input("Enter an integer:"))
-print("Factors are:")
-i=0
-for(i=0;i<n;i++)
-while(i<=n):
+num=int(input("Enter an integer:"))
+print("Factors of the integer are:")
+i=1
+while(i<=num):
     k=0
-    if(n%i==0):
+    if(num%i==0):
         j=1
         while(j<=i):
-            if(i%j==1):
+            if(i%j==0):
                 k=k+1
             j=j+1
         if(k==2):
             print(i)
     i=i+1
+
+
+    
+   
